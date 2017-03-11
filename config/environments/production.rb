5f76226e346dd42967270168d3f7d5aecbd4859e
Mohsen Sadeghbeigi <mohsen.sadegh62@gmail.com>
2017-03-11 13:06:36 +0330

    added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,15 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'shetabyad.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :domain               => 'shetabyad.com',
-      :user_name            => 'shetabyad@gmail.com',
-      :password             => ':password',
-      :authentication       => :plain,
-      :enable_starttls_auto => true  
-  }
+  
 end
